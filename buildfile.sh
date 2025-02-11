93c5a032d162030c3e2484e5636cd167d0932b06
gmsadmin-git <tech@graytitude.com>
2025-02-11 21:57:48 +0530

    Update buildfile.sh

--- buildfile.sh
+++ buildfile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "--------------------"
 echo "creating buildlist"
-git -C /tmp/appWorkspace/Development/MortgageApplication diff --name-only HEAD~1 HEAD | while read file; do echo "$(git rev-parse --show-toplevel)/$file"; done > /tmp/buildList.txt
+git -C /tmp/appWorkspace/Development/MortgageApplication diff --name-only HEAD~1 HEAD | awk -v repo="/tmp/appWorkspace/Development/MortgageApplication" '{print repo "/" $0}' > /tmp/buildList.txt
 sed 's|/S0W1||g' /tmp/buildList.txt > /tmp/temp.txt && mv /tmp/temp.txt /tmp/buildList.txt  
